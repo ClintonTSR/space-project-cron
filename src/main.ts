129b9a6414b6f214bbd740cad56e6d6998af7bb2
ClintonTSR <soonren20@icloud.com>
2023-10-31 21:00:34 +0800

    fix: rmq not initialised

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import Decimal from "decimal.js";
 import { DECIMAL_PRECISION } from "./common/constants/config.contants";
 
 async function bootstrap() {
-    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {});
+    const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+        transport: Transport.RMQ,
+        options: {
+            urls: [process.env.RMQ_URL!],
+            queue: 'pi-result',
+            queueOptions: {
+                durable: false
+            }
+        }
+    });
 
     Decimal.config({ precision: DECIMAL_PRECISION })
     await app.listen();
